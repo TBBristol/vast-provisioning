a2fedc8c231f016a1592c848893405a0e3385494
Tim Broadhurst <ha24583@bristol.ac.uk>
2025-12-24 16:51:21 +0000

    Enhance puffer_prov.sh with logging and error handling
    Add error handling and logging to the script

--- puffer_prov.sh
+++ puffer_prov.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -ex
 exec > >(tee /workspace/provision.log) 2>&1
+
+# Cause the script to exit on failure.
+set -eo pipefail
+
+# Activate the main virtual environment
+. /venv/main/bin/activate
+
 #
 cd /workspace/
 git clone https://github.com/PufferAI/PufferLib.git
